a850f3882b066f82383cab1d5ce0b533cfad2085
Erik Zhong <z3462813@student.unsw.edu.au>
2018-02-27 23:23:49 +1100

    Got basic reactjs template compiling

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/* eslint-disable import/default */
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+require('./favicon.ico');
+import './styles.scss';
+import 'font-awesome/css/font-awesome.css';
+import 'flexboxgrid/css/flexboxgrid.css';
+
+injectTapEventPlugin();
+
+render(
+    <Router routes={routes} history={browserHistory} />, document.getElementById('app')
+);
