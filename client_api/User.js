35d73e5d8ff931884b89929b9f7ecb715ea9c31a
Erik Zhong <z3462813@student.unsw.edu.au>
2018-03-08 23:33:37 +1100

    Created a user api for the gui

---
+++ client_api/User.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// connection configurations
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'erikz',
+  password: 'ADASDxcaq2',
+  database: 'table_base_gui'
+});
+
+// connect to database
+db.connect();
+
+// default route
+app.get('/', function (req, res) {
+  return res.send({ error: true, message: 'Please enter a valid URL' })
+});
+
+app.get('/user/:id', function (req, res) {
+  let userId = req.params.id;
+
+  if (!userId) {
+    return res.status(400).send({error: true, message: 'Please enter a valid user id'})
+  }
+
+  db.query("SELECT * FROM users WHERE user_id = ?", userId, function (error, results, fields) {
+    if(error) throw error;
+    return res.send({error: false, data: results[0]})
+  });
+});
+
+app.post('/user', function (req, res) {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+
+  if(!username || !email || !password){
+    return res.status(400).send({error: true, message: 'Please provide a user'})
+  }
+
+  db.query("INSERT INTO users (username, email, password) VALUES(?,?,?)", [username, email, password], function (error, results, field) {
+    if (error) throw error;
+    return res.send({ error: false, data: results, message: 'User has been added'})
+  })
+});
+
+// port must be set to 8080 because incoming http requests are routed from port 80 to port 8080
+app.listen(8081, function () {
+  console.log('Node app is running on port 8081');
+});
+
+// allows "grunt dev" to create a development server with livereload
+module.exports = app;
